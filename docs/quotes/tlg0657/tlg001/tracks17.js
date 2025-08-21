39bf4595f991c789abe121da742c5284fe16e02e
David Smith <dasmiq@gmail.com>
2025-08-21 09:55:21 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg0657/tlg001/tracks17.js
@@ -0,0 +1,2 @@
+var tracks17 =
+[{"id":"32044088787056","name":"Kampouroglous, Dēmētrios Grēgoriou, Mnēmeia tēs historias tōn Athēnaiōn [1891]","lang":"ell","date":"1891","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":2695,"stop":2715,"pre":"","id":1382979794044,"url":"32044088787056&seq=509","sloc":"p. 509"}]}];
---
+++ docs/quotes/tlg0657/tlg001/tracks9.js
@@ -0,0 +1,2 @@
+var tracks9 =
+[{"id":"32044088809363","name":"Xenos, Stephanos Th., Hē hērōïs tēs Hellēnikēs epanastaseōs, ē... [1886]","lang":"ell","date":"1886","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":3808,"stop":3827,"pre":"","id":919123326331,"url":"32044088809363&seq=270","sloc":"p. 270"}]}];
