7919ac0432170a4cb32367dbff0f24f990ed0b62
David Smith <dasmiq@gmail.com>
2025-08-21 09:52:44 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg0618/tlg001/tracks14.js
@@ -0,0 +1,2 @@
+var tracks14 =
+[{"id":"HXIJXB","name":"Cauer, Paul, Delectus inscriptionum graecarum propter... [1883]","lang":"ell","date":"1883","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":0,"stop":22,"pre":"","id":850403619220,"url":"hxijxb&seq=294","sloc":"p. 294"}]}];
