ef8626e3cbe2321a7049562ca5a09e73157a7942
David Smith <dasmiq@gmail.com>
2025-08-21 10:04:53 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg1337/tlg003/tracks14.js
@@ -0,0 +1,2 @@
+var tracks14 =
+[{"id":"HXJVAC","name":"Flach, Hans., Das system der hesiodischen Kosmogonie. [1874]","lang":"deu","date":"1874","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":2149,"stop":2170,"pre":"","id":472446417708,"url":"hxjvac&seq=23","sloc":"p. 23"}]}];
