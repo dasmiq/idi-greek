2208f77258e821f8c0bab4a199fa9668e965e8df
David Smith <dasmiq@gmail.com>
2025-08-21 10:08:27 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg1805/tlg007/tracks14.js
@@ -0,0 +1,2 @@
+var tracks14 =
+[{"id":"HXJHBQ","name":"Fragmenta historicorum graecorum [1841]","lang":"lat","date":"1841","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":258,"stop":282,"pre":"","id":455266562825,"url":"hxjhbq&seq=627","sloc":"p. 627"}]}];
---
+++ docs/quotes/tlg1805/tlg007/tracks2.js
@@ -0,0 +1,2 @@
+var tracks2 =
+[{"id":"32044100906569","name":"Rankavēs, Alexandros Rizos, Hapanta : ta philologika [1874]","lang":"ell","date":"1874","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":235,"stop":282,"pre":"","id":1254130714762,"url":"32044100906569&seq=690","sloc":"p. 690"}]}];
