b4d9de4614cf3729a2ccc7cbf3ea4fe6f94a1ad9
David Smith <dasmiq@gmail.com>
2025-08-21 10:25:19 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg2189/tlg003/tracks14.js
@@ -0,0 +1,2 @@
+var tracks14 =
+[{"id":"HXJHBQ","name":"Fragmenta historicorum graecorum [1841]","lang":"lat","date":"1841","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":2,"notes":[{"start":0,"stop":1040,"pre":"","id":2044404780165,"url":"hxjhbq&seq=675","sloc":"p. 675"},{"start":1040,"stop":1314,"pre":"","id":188978908532,"url":"hxjhbq&seq=676","sloc":"p. 676"}]}];
