ad876a68d105176dcd27f9b0c1de42315ddd0d12
David Smith <dasmiq@gmail.com>
2025-08-21 09:47:09 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg0542/tlg001/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "2", "name": "2", "start": 0, "stop": 890, "children": [{"id": "2.157", "name": "157", "start": 0, "stop": 496}, {"id": "2.158", "name": "158", "start": 497, "stop": 890}]}]}];
---
+++ docs/quotes/tlg0542/tlg001/tracks1.js
@@ -0,0 +1,2 @@
+var tracks1 =
+[{"id":"HWSGG9","name":"Liddell, Henry George, Mega lexikon tēs Hellēnikēs glōssēs : ep... [1921]","lang":"ell","date":"1921","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":309,"stop":385,"pre":"","id":1623497903389,"url":"hwsgg9&seq=353","sloc":"p. 353"}]}];
