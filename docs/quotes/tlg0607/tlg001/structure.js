7415a3ff91588c5acc34f9b3bf07b79d88c2841a
David Smith <dasmiq@gmail.com>
2025-08-21 09:51:45 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg0607/tlg001/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "1", "name": "1", "start": 0, "stop": 12261}, {"id": "2", "name": "2", "start": 12262, "stop": 25569}, {"id": "3", "name": "3", "start": 25570, "stop": 36503}, {"id": "4", "name": "4", "start": 36504, "stop": 71312}, {"id": "5", "name": "5", "start": 71313, "stop": 87928}]}];
