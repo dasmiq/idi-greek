6158842b0ba050d705b4376ce01ce59c443577b7
David Smith <dasmiq@gmail.com>
2025-08-21 09:54:27 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg0643/tlg002/tracks17.js
@@ -0,0 +1,2 @@
+var tracks17 =
+[{"id":"32044011261740","name":"Neuburger, Max, Geschichte der Medizin. [1906]","lang":"deu","date":"1906","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":0,"stop":20,"pre":"","id":463856682135,"url":"32044011261740&seq=178","sloc":"p. 178"}]}];
