9887395c20ed0ac6bdeed4e768cd74d48a656e0a
David Smith <dasmiq@gmail.com>
2025-08-21 10:26:46 -0400

    Adding works incrementally.

---
+++ docs/quotes/tlg2200/tlg00444/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "1", "name": "1", "start": 0, "stop": 1026}, {"id": "3", "name": "3", "start": 1027, "stop": 1521}, {"id": "4", "name": "4", "start": 1522, "stop": 2833}]}];
---
+++ docs/quotes/tlg2200/tlg00460/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "1", "name": "1", "start": 0, "stop": 2929}, {"id": "8", "name": "8", "start": 2930, "stop": 3571}, {"id": "9", "name": "9", "start": 3572, "stop": 4124}, {"id": "10", "name": "10", "start": 4125, "stop": 6001}, {"id": "13", "name": "13", "start": 6002, "stop": 6719}]}];
